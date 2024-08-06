e51dafdc5f4576b1ab200218b9c396b9fabfba6a
刘赵杰 <2817274709@qq.com>
2024-08-06 18:45:21 +0800

    Site updated: 2024-08-06 18:45:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["折腾Markdown/use-markdown1/","折腾Markdown/use-markdown3/","折腾Markdown/use-markdown2/","折腾Hexo/use-hexo1/","元启发式优化/metaheuristic/","元启发式优化/SA1/","元启发式优化/TS1/","元启发式优化/GA1/","元启发式优化/ACA1/"];function toRandomPost(){
+var posts=["折腾Markdown/use-markdown1/","折腾Markdown/use-markdown3/","折腾Markdown/use-markdown2/","折腾Hexo/use-hexo1/","元启发式优化/metaheuristic/","元启发式优化/SA1/","元启发式优化/TS1/","元启发式优化/GA1/","元启发式优化/ACA1/","电视剧/Horror Stories of Tang Dynasty(1)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
