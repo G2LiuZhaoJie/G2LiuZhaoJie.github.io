0a925741c00e4784bdfb93519e7162cd604242ab
刘赵杰 <2817274709@qq.com>
2024-08-27 05:26:47 +0800

    Site updated: 2024-08-27 05:26:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["折腾Markdown/use-markdown1/","折腾Markdown/use-markdown3/","折腾Markdown/use-markdown2/","折腾Hexo/use-hexo1/","元启发式优化/metaheuristic/","元启发式优化/SA1/","元启发式优化/TS1/","元启发式优化/GA1/","元启发式优化/ACA1/","电视剧/Horror Stories of Tang Dynasty(1)/","algorithm/双向搜索/","algorithm/启发式搜索/","algorithm/A+搜索/"];function toRandomPost(){
+var posts=["折腾Markdown/use-markdown1/","折腾Markdown/use-markdown3/","折腾Markdown/use-markdown2/","折腾Hexo/use-hexo1/","元启发式优化/metaheuristic/","元启发式优化/SA1/","元启发式优化/TS1/","元启发式优化/GA1/","元启发式优化/ACA1/","电视剧/Horror Stories of Tang Dynasty(1)/","algorithm/双向搜索/","algorithm/启发式搜索/","algorithm/迭代加深搜索/","algorithm/Dancing Links/","algorithm/Astar/","algorithm/IDAstar/","algorithm/Alpha-Beta 剪枝/","algorithm/优化/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
